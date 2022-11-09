120a534e6ceeb8befb4b4cbe11fb27b905cfaaca
hir0o <mk.9.q.kyu@gmail.com>
2022-11-09 17:22:49 +0900

    init

---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react';
+
+export const Button: FC = () => {
+  return (
+    <button
+      type="button"
+      style={{
+        backgroundColor: 'blue',
+        color: 'white',
+        padding: '10px 20px',
+        borderRadius: '5px',
+        border: 'none',
+        cursor: 'pointer',
+      }}
+    >やああ</button>
+  )
+};
